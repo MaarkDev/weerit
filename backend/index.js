541b867eca1168bb24e21393628b4742f5de3547
Mark <busdevmark@gmail.com>
2023-06-19 18:07:01 +0200

    changed api url

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(passport.session());
 app.use(cors({
   origin: [process.env.FRONTEND_URL, process.env.BACKEND_URL],
   methods: 'GET,POST,PUT,DELETE,PATCH',
-  credentials: true
+  credentials: true 
 }));
 
 app.use('/auth', authRoute);
