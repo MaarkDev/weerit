01a6308315db3f762d952a77ce8d767b6ee041c5
Mark <busdevmark@gmail.com>
2023-10-03 15:03:29 +0200

    new commit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const passport = require('passport')
 const passportCfg = require('./passport')
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL],
+  origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_NOWWW],
   methods: 'GET,POST,PUT,DELETE,PATCH',
   credentials: true
 }));
